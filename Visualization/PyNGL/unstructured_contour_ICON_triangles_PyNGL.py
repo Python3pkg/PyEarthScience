a8cd3aa2ff80442e9f0f11a62709c915df1279c8
Karin Meier-Fleischer <meier-fleischer@dkrz.de>
2016-02-19 14:43:50 +0100

    Update unstructured_contour_ICON_triangles_PyNGL.py
    Optimize code

--- Visualization/PyNGL/unstructured_contour_ICON_triangles_PyNGL.py
+++ Visualization/PyNGL/unstructured_contour_ICON_triangles_PyNGL.py
@@ -1,246 +1,196 @@
 """
-  PyEarthScience: PyNGL vector example
-
-    - ICON data
-    - colored triangles
+  PyEarthScience: PyNGL unstructured data draw polygons example
+  
+  Grid type:  unstructured
+  Model:      ICON
+  Info:       - colored triangles
+              - add labelbar (colorbar)
+              - wksColorMap
+    
+  The Ngl.polygon function is used because it's faster! 
  
- 04.06.15  kmf
+ 18.02.16    meier-fleischer(at)dkrz.de
 """
 import numpy as np
-import math, time
-import Ngl,Nio
+import math, time, sys, os
+import Nio, Ngl
 
-t1 = time.time()                                   #-- retrieve start time
-print ""
+t1 = time.time()                                    #-- retrieve start time
 
-#--  define variables
-diri   = "/Users/k204045/NCL/PyNGL/User_Guide_examples/" #-- data path
-fname  = "ta_ps_850.nc"                            #-- data file
-gname  = "r2b4_amip.nc"                            #-- grid info file
-VarName = "ta"                                     #-- variable name       
+#--  define variables 
+diri, fname  = '$HOME/data/ICON/', 'ta_ps_850.nc'   #-- data path and file name
+gname        = 'grids/r2b4_amip.nc'                 #-- grid info file
+VarName      = 'ta'                                 #-- variable name       
+ 
+#--  open file and read variables 
+f = Nio.open_file(diri + fname,'r')                 #-- add data file
+g = Nio.open_file(diri + gname,'r')                 #-- add grid file (not contained in data file!!!)
+ 
+#-- read a timestep of 'ta'  
+variable =  f.variables['ta']                       #-- first time step, lev, ncells
+data     =  variable[0,0,:]                         #-- ta [time,lev,ncells]; miss _FillValue
+var      =  data - 273.15                           #-- convert to degrees Celsius; miss _FillValue
 
-#--  open file and read variables
-f = Nio.open_file(diri + fname,"r")                #-- add data file
-g = Nio.open_file(diri + gname,"r")                #-- add grid file (not contained in data file!!!)
+#-- define _FillValue and missing_value if not existing
+missing = -1e20
 
-#-- read a timestep of "ta" 
-variable =  f.variables["ta"]                      #-- first time step, lev, ncells
-var      =  variable[0,0,:]                        #-- ta [time,lev,ncells]
-var      =  var - 273.15                           #-- convert to degrees Celsius
+if not hasattr(var,'_FillValue'):
+   var._FillValue  =  missing                       #-- set _FillValue
+if not hasattr(var,'missing_value'): 
+   var.missing_value =  missing                     #-- set missing_value
 
-#-- check if missing value set
-if not hasattr(var,"_FillValue"):
-   var._FillValue    =  np.array(1.e20,dtype='f')  #-- set _FillValue
-if not hasattr(var,"missing_value"):
-   var.missing_value =  np.array(1.e20,dtype='f')  #-- set missing_value
+varM = np.ma.array(var, mask=np.equal(var,missing)) #-- mask array with missing values 
+nummissing = np.count_nonzero(varM.mask)            #-- number of missing values
 
-print "-----------------------"
-print f.variables["ta"]                            #-- like printVarSummary
-print "-----------------------"
+#-- set data intervals, levels, labels, color indices
+varMin, varMax, varInt = -32, 28, 4                 #-- set data minimum, maximum, interval
+ 
+levels   =  range(varMin,varMax,varInt)             #-- set levels array
+nlevs    =  len(levels)                             #-- number of levels
+labels   =  ['{:.2f}'.format(x) for x in levels]    #-- convert list of floats to list of strings
+colors   =  range(2,nlevs+6)                        #-- create color indices
 
-title    = "ICON:  Surface temperature"            #-- title string
-varMin   = -32                                     #-- data minimum
-varMax   =  28                                     #-- data maximum
-varInt   =   4                                     #-- data increment
-levels   =  range(varMin,varMax,varInt)            #-- set levels array
-
+#-- print info to stdout
+print ''
+print 'min/max:          %.2f' %np.min(varM) + ' /' + ' %.2f' %np.max(varM)
+print ''
+print 'varMin:           %3d' %varMin
+print 'varMax:           %3d' %varMax
+print 'varInt:           %3d' %varInt
+print ''
+print 'missing_value:    ', missing
+print 'missing values:   ', nummissing
 #-------------------------------------------------------------------
 #-- define the x-, y-values and the polygon points
 #-------------------------------------------------------------------
-rad2deg = 45./np.arctan(1.)                        #-- radians to degrees
+rad2deg = 45./np.arctan(1.)                         #-- radians to degrees
 
-x      =  g.variables["clon"][:]
-y      =  g.variables["clat"][:]
-vlon   =  g.variables["clon_vertices"][:]
-vlat   =  g.variables["clat_vertices"][:]
+x, y       =  g.variables['clon'][:], g.variables['clat'][:]
+vlon, vlat =  g.variables['clon_vertices'][:], g.variables['clat_vertices'][:]
 
-ncells =  vlon.shape[0]                            #-- number of cells
-nv     =  vlon.shape[1]                            #-- number of edges
+x, y       =  x*rad2deg,  y*rad2deg                 #-- cell center, lon, lat
+vlat, vlon =  vlat*rad2deg, vlon * rad2deg          #-- cell latitude/longitude vertices
+ncells, nv =  vlon.shape                            #-- ncells: number of cells; nv: number of edges
 
-x      =  x    * rad2deg                           #-- cell center, lon
-y      =  y    * rad2deg                           #-- cell center, lat
-vlat   =  vlat * rad2deg                           #-- cell lattitude vertices
-vlon   =  vlon * rad2deg                           #-- cell longitude vertices
+#-- print information to stdout
+print ''
+print 'cell points:      ', nv
+print 'cells:            ', str(ncells)
+print ''
 
-#-- longitude values -180. - 180.
-for j in xrange(1,ncells):
-    for i in range(1,nv):
-        if vlon[j,i] < -180. :
-           vlon[j,i] = vlon[j,i] + 360.
-        if vlon[j,i] > 180. :
-           vlon[j,i] = vlon[j,i] - 360.
+#-- rearrange the longitude values to -180.-180.
+def rearrange(vlon):
+    less_than    = vlon < -180.
+    greater_than = vlon >  180.
+    vlon[less_than]    = vlon[less_than] + 360.
+    vlon[greater_than] = vlon[greater_than] - 360.
+    return vlon
 
-#-- information
-print ""
-print "Cell points:           ", nv
-print "Cells:                 ", str(ncells)
-print "Variable ta   min/max:  %.2f " % np.min(var) + "/" + " %.2f" % np.max(var)
-print ""
+vlon = rearrange(vlon)                              #-- set longitude values to -180.-180. degrees
 
-#-- open workstation for first plot: contour plot
-wks_type = "png"                                   #-- output type
-wks      =  Ngl.open_wks(wks_type,"plot_unstructured_ICON_triangles_1_PyNGL") #-- open workstation
-
-#-- contour plot resources
-res                      =  Ngl.Resources()        #-- plot mods desired.
-res.nglDraw              =  False                  #-- turn off plot draw and frame advance. We will
-res.nglFrame             =  False                  #-- do it later after adding subtitles.
-
-res.cnFillOn             =  True                   #-- color plot desired
-res.cnFillPalette        = "BlueWhiteOrangeRed"    #-- choose colormap
-res.cnLinesOn            =  False                  #-- turn off contour lines
-res.cnLineLabelsOn       =  False                  #-- turn off contour labels
-res.cnLevelSelectionMode = "ExplicitLevels"        #-- set explicit levels
-res.cnLevels             =  levels                 #-- set levels
-
-res.lbOrientation        = "Horizontal"            #-- vertical by default
-res.lbBoxLinesOn         =  False                  #-- turn off labelbar boxes
-res.lbLabelFontHeightF   =  0.01                   #-- labelbar label font height
-
-res.mpFillOn             =  False                  #-- don't draw map outlines
-res.mpGridAndLimbOn      =  False                  #-- don't draw grid lines
-
-res.cnFillMode           = "CellFill"              #-- set fill mode
-res.sfXArray             =  x                      #-- transform x to mesh scalar field
-res.sfYArray             =  y                      #-- transform y to mesh scalar field
-res.sfXCellBounds        =  vlon                   #-- needed if set cnFillMode = "CellFill"
-res.sfYCellBounds        =  vlat                   #-- needed if set cnFillMode = "CellFill"
-
-res.tiMainString         = "ICON grid - raster fill"
-res.tiMainOffsetYF       =  0.03                   #-- move main title towards plot
-
-#-- create the contour plot to retrieve levels and colors
-plot = Ngl.contour_map(wks,var,res)
-
-Ngl.draw(plot)
-Ngl.frame(wks)
-del(levels)
-
-#========== second plot ============
-
-#-- retrieve the colors and levels from contour plot
-levels   =  list(Ngl.get_float_array(plot.contour,"cnLevels"))
-labels   =  list(Ngl.get_string_array(plot.contour,"cnLevels"))
-nlevels  =  len(levels)
-
-#-- define colormap (RGB value range 0-255)
-colormap255 = np.array([[  0,   0,   0], [255, 255, 255], [ 80, 255, 255], \
-                        [ 27,  44,  98], [ 45, 102, 175], [ 66, 133, 196], \
-                        [ 90, 166, 217], [119, 189, 230], [148, 211, 243], \
-                        [215, 239, 249], [237, 248, 252], [255, 255, 255], \
-                        [254, 248, 218], [253, 208, 107], [253, 167,  49], \
-                        [247, 124,  43], [231,  75,  41], [203,  30,  38], \
-                        [146,  21,  25]], int)
-
-ncol     =  colormap255.shape[0]                     #-- number of colors
-colormap =  np.array(colormap255)/255.               #-- convert to RGB value range 0.0-1.0
+print 'min/max vlon:     ', np.min(vlon), np.max(vlon)
+print 'min/max vlat:     ', np.min(vlat), np.max(vlat)
+print ''
 
 #-- open a workstation for second plot:  triangles plot
-wkres            =  Ngl.Resources()
-wkres.wkColorMap =  colormap                       #-- choose colormap
-wkres.wkWidth    =  2500                           #-- width of output graphic
-wkres.wkHeight   =  2500                           #-- height of output graphic
-wks1_type        = "png"                           #-- output type
-wks1             =  Ngl.open_wks(wks1_type,"plot_unstructured_ICON_triangles_2_PyNGL",wkres) #-- open workstation
+wkres =  Ngl.Resources()
+wkres.wkColorMap =  'WhiteBlueGreenYellowRed'       #-- choose colormap
+wkres.wkWidth, wkres.wkHeight  =  2500, 2500
+wks1_type = 'png'
+wks1 =  Ngl.open_wks(wks1_type,'Py_unstructured_ICON_triangles',wkres)
 
+#-- define colormap
+cmap     =  Ngl.retrieve_colormap(wks1)             #-- RGB ! [256,3]
+ncmap    =  cmap.shape[0]                           #-- number of colors
+colormap =  cmap[:ncmap:12,:]                       #-- select every 13th color 
+ncol     =  colormap.shape[0]
+colormap[20] = ([1.,1.,1.])                         #-- white for missing values
+
+print 'colors index:     ', colors 
+print ''
+print 'levels:           ',levels
+print 'labels:           ',labels
+print ''
+print 'nlevs:            %3d' %nlevs
+print 'ncols:            %3d' %ncol
+print ''
+
+#-- overwrite resources of wks1
+setlist                    =  Ngl.Resources()
+setlist.wkColorMap         =  colormap              #-- set color map to new colormap array
+setlist.wkBackgroundColor  = 'white'                #-- has to be set when wkColorMap is set to colormap array
+setlist.wkForegroundColor  = 'black'                #-- has to be set when wkColorMap is set to colormap array
+Ngl.set_values(wks1,setlist)
+  
 #-- set map resources
 mpres                             =  Ngl.Resources()
-mpres.nglDraw                     =  False         #-- turn off plot draw and frame advance. We will
-mpres.nglFrame                    =  False         #-- do it later after adding subtitles.
+mpres.nglDraw                     =  False          #-- turn off plot draw and frame advance. We will
+mpres.nglFrame                    =  False          #-- do it later after adding subtitles.
+mpres.mpGridAndLimbOn             =  False
+mpres.mpGeophysicalLineThicknessF =  2.
+mpres.pmTitleDisplayMode          = 'Always'
+mpres.tiMainString                = 'PyNGL: unstructured grid ICON'
 
-mpres.mpGridAndLimbOn             =  False         #-- don't draw grid lines
-mpres.mpGeophysicalLineThicknessF =  2.            #-- map outlines thicker
-#mpres.pmTitleDisplayMode          = "Always"
-
-#mpres.mpLimitMode                 = "LatLon"      #-- map limit mode
-#mpres.mpMinLonF                   = -60.          #-- minimum lon 
-#mpres.mpMaxLonF                   =  60.          #-- maximum lon 
-#mpres.mpMinLatF                   =   0.          #-- minimum lat
-#mpres.mpMaxLatF                   =  90.          #-- maximum lat 
-
-mpres.tiMainString                = "ICON data - gsSegments" #-- title string
-
-#-- create map
+#-- create only a map
 map = Ngl.map(wks1,mpres)
-Ngl.draw(map)                                     #-- draw the empty map
+Ngl.draw(map)
 
-#-- assign array which will hold the color indices of the cells
-colors    =  range(3,ncol+3)                       #-- create color indices
-gscolors  =  np.ones((ncells,),dtype=np.int)       #-- assign array containing zeros
-gscolors  =  -1*np.array(gscolors)                 #-- initialize to transparent color
+#-- assign and initialize array which will hold the color indices of the cells
+gscolors = -1*(np.ones((ncells,),dtype=np.int))     #-- assign array containing zeros; init to transparent: -1
 
-#-- lowest level polygons
-vlow = Ngl.ind(var < levels[0])
-for k in xrange(0,len(vlow)-1):
-    if not np.all(var[vlow] == var._FillValue):
-       for kk in vlow:
-           gscolors[kk] = 3  
-print "finished level 0 -- " + str(len(vlow)) + " polygons considered - gscolors " + str(colors[0])
-
-#-- all cells in between levels
-for m in xrange(0,nlevels-1):
-    vind = []                                      #-- empty list for color indices
+#-- set color index of all cells in between levels
+for m in xrange(0,nlevs):
+    vind = []                                       #-- empty list for color indices
     for i in xrange(0,ncells-1):
-        if (var[i] >= levels[m] and var[i] < levels[m+1]):
-           gscolors[i] = colors[m]
+        if (varM[i] >= levels[m] and varM[i] < levels[m+1]):
+           gscolors[i] = colors[m+1]
            vind.append(i)
-        if (var[i] == var._FillValue or var[i] == var.missing_value):
-           gscolors[i] = 2
-
-    print "finished level " + str(m) + " -- " + str(len(vind)) + " polygons considered - gscolors " + str(colors[m])
+    print 'finished level %3d' % m , ' -- %5d ' % len(vind) , ' polygons considered - gscolors %3d' % colors[m]
     del vind
 
-#-- all cells higher than the highest level
-vhgh = Ngl.ind(var >= levels[nlevels-1])
-
-for n in xrange(0, len(vhgh)-1):
-    if not np.all(var[vhgh] == var._FillValue):
-       for nn in vhgh:
-           gscolors[nn] = colors[nlevels-1]
-          
-print "finished level " + str(nlevels) + " -- " + str(len(vhgh)) + " polygons considered - gscolors " + str(colors[nlevels-1])
-print "++ gscolors:     ", gscolors[:]
-print "++ min/max vlon: ", np.min(vlon), np.max(vlon)
-print "++ min/max vlat: ", np.min(vlat), np.max(vlat)
+gscolors[varM < varMin]         =  colors[0]        #-- set color index for cells less than level[0]
+gscolors[varM >= varMax]        =  colors[(nlevs-1)+2] #-- set color index for cells greater than levels[nlevs-1]
+gscolors[np.nonzero(varM.mask)] =  20               #-- set color index for missing values
 
 #-- set polygon resources
-pgres                             =  Ngl.Resources()
-pgres.gsEdgesOn                   =  True                 #-- draw the edges
-pgres.gsFillIndex                 =  0                    #-- solid fill
-pgres.gsLineColor                 = "black"               #-- edge line color
-pgres.gsLineThicknessF            =  0.7                  #-- line thickness
-pgres.gsColors                    =  gscolors             #-- use color array
-pgres.gsSegments                  =  range(0,len(vlon[:,0])*3,3)  #-- define segments array
+pgres                   =  Ngl.Resources()
+pgres.gsEdgesOn         =  True                     #-- draw the edges
+pgres.gsFillIndex       =  0                        #-- solid fill
+pgres.gsLineColor       = 'black'                   #-- edge line color
+pgres.gsLineThicknessF  =  0.7                      #-- line thickness
+pgres.gsColors          =  gscolors                 #-- use color array
+pgres.gsSegments        =  range(0,len(vlon[:,0])*3,3) #-- define segments array
 
-x1d  =  np.ravel(vlon)                                    #-- 1D-array
-y1d  =  np.ravel(vlat)                                    #-- 1D-array
+x1d, y1d = np.ravel(vlon), np.ravel(vlat)           #-- convert to 1D-arrays
 
-#-- draw polygons
-polyg  = Ngl.add_polygon(wks1,map,x1d,y1d,pgres)          #-- add polygons to map
+#-- add polygons to map
+polyg  = Ngl.add_polygon(wks1,map,x1d,y1d,pgres)
 
-#-- labelbar resources
-lbres                             =  Ngl.Resources()
-lbres.vpWidthF                    =  0.85                 #-- labelbar width
-lbres.vpHeightF                   =  0.15                 #-- labelbar height
-lbres.lbOrientation               = "Horizontal"          #-- labelbar orientation
-lbres.lbFillPattern               = "SolidFill"           #-- labelbar box fill style
-lbres.lbMonoFillPattern           =  21                   #-- labelbar box fill pattern
-lbres.lbMonoFillColor             =  False                #-- use multiple colors
-lbres.lbFillColors                =  colors               #-- set color array
-lbres.lbLabelFontHeightF          =  0.014                #-- labelbar label font size
-lbres.lbLabelAlignment            = "InteriorEdges"       #-- draw labels between boxes
+#-- add a labelbar
+lbres                   =  Ngl.Resources()
+lbres.vpWidthF          =  0.85
+lbres.vpHeightF         =  0.15
+lbres.lbOrientation     = 'Horizontal'
+lbres.lbFillPattern     = 'SolidFill'
+lbres.lbMonoFillPattern =  21                       #-- must be 21 for color solid fill
+lbres.lbMonoFillColor   =  False                    #-- use multiple colors
+lbres.lbFillColors      =  colors                   #-- indices from loaded colormap
+lbres.lbBoxCount        =  len(colormap[colors,:])
+lbres.lbLabelFontHeightF=  0.014
+lbres.lbLabelAlignment  = 'InteriorEdges'
+lbres.lbLabelStrings    =  labels
 
-#-- add labelbar to workstation
-lb = Ngl.labelbar_ndc(wks1,nlevels,labels,0.1,0.2,lbres)
+lb = Ngl.labelbar_ndc(wks1,nlevs+1,labels,0.1,0.24,lbres)
 
-#-- maximize and draw the plot, and advance the frame
+#-- maximize and draw the plot and advance the frame
 Ngl.maximize_plot(wks1, map)
 Ngl.draw(map)
 Ngl.frame(wks1)
 
 #-- get wallclock time
 t2 = time.time()
-print "Wallclock time:  %0.3f seconds" % (t2-t1)
-print ""
+print ''
+print 'Wallclock time:  %0.3f seconds' % (t2-t1)
+print ''
 
 Ngl.end()
